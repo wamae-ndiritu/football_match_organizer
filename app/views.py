49a31979ef2ec92ce257a836ee8aac025c012202
wamae-ndiritu <wamaejoseph392@gmail.com>
2025-03-25 07:25:30 +0300

    Add auth forms

--- app/views.py
+++ app/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'dashboard.html', {'page': {'title': 'Dashboard'}})
+
+def register_team(request):
+    return render(request, 'register.html', {'page': {'title': 'Register'}})
+
+def login(request):
+    return render(request, 'login.html', {'page': {'title': 'Login'}})
